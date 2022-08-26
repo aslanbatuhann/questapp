eb9acba5ea8fa7ed4a971afc8ffe50a2879ccdcf
Batuhan Aslan <batuhanaslan9601@gmail.com>
2022-08-26 18:32:07 +0300

    hibernate mapping

--- src/main/java/com/project/questapp/entity/Post.java
+++ src/main/java/com/project/questapp/entity/Post.java
@@ -1,6 +1,9 @@
 package com.project.questapp.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -11,7 +14,12 @@ public class Post {
 
     @Id
     Long id;
-    Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    User user;
+
     String title;
     @Lob
     @Column(columnDefinition = "text")
